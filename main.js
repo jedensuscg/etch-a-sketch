bd750f669bcf404282e1221e30d6f31d7400173c
James Edens <jedensuscg@gmail.com>
2019-10-06 20:25:26 -0400

    Finished initial coding. Can draw, erase and resize board

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 let container = document.getElementById("container");
 let gridSize = 16;
-generateGrid(gridSize);
+let boardSize = 400;
+
+start();
+
+function start(){
+    generateGrid(gridSize);
+    fillSquare();
+    showInfo();
+}
+
 document.getElementById("erase").addEventListener("click", eraseSquares)
-function generateGrid(gridSize)
-{
-    for(var i = 0; i < gridSize; i++){
+function generateGrid(currentGridSize) {
+    console.log("grid size: " + currentGridSize)
+    for (let i = 0; i < currentGridSize; i++) {
         let row = document.createElement("div");
         row.className = "row";
-        for(var x = 0; x <= gridSize; x++){
+        for (let x = 0; x <= currentGridSize; x++) {
             let cell = document.createElement("div")
             cell.className = "gridSquare";
             row.appendChild(cell);
@@ -17,21 +26,46 @@ function generateGrid(gridSize)
 }
 fillSquare();
 
-function fillSquare(){
+function fillSquare() {
     let squares = document.querySelectorAll(".gridSquare");
     squares.forEach((sqaure) => {
-        sqaure.addEventListener('mouseover', (e) =>{
+        sqaure.addEventListener('mouseover', (e) => {
             e.target.classList.add("gridSquareHover");
         })
     })
 }
 
-function eraseSquares(){
+function eraseSquares() {
     let squares = document.querySelectorAll(".gridSquare");
     squares.forEach((square) => {
-        if (square.classList.contains("gridSquareHover")){
-        square.classList.remove("gridSquareHover");
-    }
-})
+        if (square.classList.contains("gridSquareHover")) {
+            square.classList.remove("gridSquareHover");
+        }
+    })
+    changeSize();
+}
 
+function changeSize() {
+    let grid = document.getElementById("container");
+    boardSize = document.getElementById("squareSize").value;
+    grid.innerHTML = "";
+    gridSize = newSize = document.getElementById("gridSize").value;
+    console.log(changeSize)
+    grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+    grid.style.setProperty('--grid-squares', gridSize);
+    start();
+
+    boardSize = boardSize +"px";
+    grid.style.setProperty('--grid-size', boardSize);
+
+}
+
+function showInfo(){
+    var resolutionLabel = document.getElementById("resolutionInfo");
+    var boardSizeLabel = document.getElementById("boardSize");
+    var squareSizeLabel = document.getElementById("totalSquares");
+    resolutionLabel.textContent = `Board Resolution: ${gridSize} X ${gridSize} squares`
+    boardSizeLabel.textContent = `Board Size: ${boardSize} X ${boardSize} pixels`
+    squareSizeLabel.textContent = `Size of Each Square: ${boardSize / gridSize} pixels`
 }
